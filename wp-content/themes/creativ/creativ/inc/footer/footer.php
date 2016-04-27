7552d5acecaab99f02c9d0aacd91f08122f181e5
Khoa <khoald2302@gmail.com>
2016-04-27 09:25:18 +0700

    page design ecommer  whoweare

--- wp-content/themes/creativ/creativ/inc/footer/footer.php
+++ wp-content/themes/creativ/creativ/inc/footer/footer.php
@@ -1,34 +1,68 @@
- 
- <?php 
- global $smof_data, $zo_meta; 
- if ($smof_data['enable_footer_top'] =='1'): ?>
-    <div id="zo-footer-top">
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package ZoTheme
+ * @subpackage Zo Theme
+ * @since 1.0.0
+ */
+global $smof_data;
+?>
+</div><!-- #main -->
+<footer>
+    <div id="footer">
         <div class="container">
             <div class="row">
-                <?php if (is_active_sidebar('sidebar-5')) : ?>
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-first"><?php dynamic_sidebar('sidebar-5'); ?></div>
-                <?php endif; ?>
-                <?php if (is_active_sidebar('sidebar-6')) : ?>
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-second"><?php dynamic_sidebar('sidebar-6'); ?></div>
-                <?php endif; ?>
-                <?php if (is_active_sidebar('sidebar-7')) : ?>
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-third"><?php dynamic_sidebar('sidebar-7'); ?></div>
-                <?php endif; ?>
-                <?php if (is_active_sidebar('sidebar-8')) : ?>
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-four"><?php dynamic_sidebar('sidebar-8'); ?></div>
-                <?php endif; ?>
+                <div class="col-sm-12">
+                    <img alt="Webpuppies Footer Logo" src="http://localhost/webpuppies/wpsg.dev/wp-content/uploads/2016/04/site-logo-footer.png">
+                    <hr>
+                </div>
+                <div class="col-sm-6" id="footer-left">
+
+                    <ul id="foot_services" class="list-unstyled">
+                        <li id="foot_header">Our Services</li>
+                        <li>
+                            <span class="fa fa-desktop fa-1g fa-pad"></span>
+                            <a href="/web-design">Web Design</a></li>
+                        <li>
+                            <span class="fa fa-shopping-cart fa-1g fa-pad"></span><a href="/ecommerce">Ecommerce</a></li>
+                        <li>
+                            <!--<span class="fa fa-usd fa-1g fa-pad"></span><a href="#">Digital Marketing</a></li>
+                            <li>
+                            <span class="fa fa-wrench fa-1g fa-pad"></span><a href="#">Maintenance</a></li>
+                            <li>
+                            <span class="fa fa-stethoscope fa-1g fa-pad"></span><a href="#">Consultation</a></li>
+                            -->
+                        </li></ul>
+
+                    <ul id="foot_links" class="list-unstyled">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/why-us">Why Us?</a></li>
+                        <li><a href="/who-we-are">Who We Are</a></li>
+                        <li><a href="/pic">PIC Grant</a></li>
+                        <li><a href="/careers">Careers</a></li>
+                        <li class="footer-btn"><a href="/contact-us">Contact Us</a></li>
+                    </ul>
+
+                </div>
+                <div class="col-sm-6" id="footer-right">
+                    <h5 class="footer-header">Locate Us</h5>
+                    <p>Trivex<br>8 Burn Road, #13-02<br>Singapore 369977</p>
+                    <p><a href="tel:+6567411708">+65 6741 1708</a><br><a href="mailto:contact@webpuppies.com.sg">contact@webpuppies.com.sg</a></p>
+                    <p>&nbsp;</p>
+                    <h5 class="footer-header">Stay Connected</h5>
+                    <a href="https://facebook.com/webpuppies.singapore" rel="nofollow" target="_blank"><span class="fa fa-facebook"><div class="hidden">Facebook</div></span></a><a href="https://twitter.com/webpuppies" rel="nofollow" target="_blank"><span class="fa fa-twitter social-icon"><div class="hidden">Twitter</div></span></a>
+                </div>
+                <div class="col-sm-12" id="copyright">
+                    <p>© 2016 Webpuppies. All Rights Reserved. <span id="terms-privacy"><a href="/terms">Terms of Service</a> &amp; <a href="/privacy">Privacy Policy</a></span></p>
+                </div>
             </div>
         </div>
     </div>
-    <?php endif;?>
-    <?php if ($smof_data['enable_footer_bottom'] =='1'): ?>
-    <div id="zo-footer-bottom">
-         <div class="container">
-             <div class="row">
-                 <?php if (is_active_sidebar('footer-bottom-vesion-one')) : ?>
-                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bottom-first"><?php dynamic_sidebar('footer-bottom-vesion-one'); ?></div>
-                 <?php endif; ?>
-             </div>
-         </div>
-    </div>
-    <?php endif;?>
+</footer><!-- #site-footer -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html>
